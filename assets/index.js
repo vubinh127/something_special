a61fbd9b904a7a9adb2abae28cda1323803c78d6
vubinh127 <81604514+vubinh127@users.noreply.github.com>
2024-03-29 16:15:51 +0700

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,20 @@
-{\rtf1}
+$(document).ready(function () {
+    "use strict"
+  
+    // $(".body").ripples({ //function to get the  ripple water effect 
+    //   dropRadius: 12,
+    //   perturbance: 0.01,
+    // });
+
+    $('#no').on('mouseover touchstart', function() {
+        var x = Math.floor(Math.random() * ($(window).width() - 150));
+        var y = Math.floor(Math.random() * ($(window).height() - 150));
+        $('#no').css({ 'left': x + 'px', 'top': y + 'px' });
+        $('#yes button').animate({ width: '+=20px' }, 'fast');
+        $('#yes button').animate({ height: '+=20px' }, 'fast');
+        $('#yes button').css('font-size', 'larger');
+      });
+
+});
+
+  
